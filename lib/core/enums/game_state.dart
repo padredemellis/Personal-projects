6831f43517fe02a34c3837cdb9234bd891736cd8
padredemellis <luisemanuelromeroduarte@gmail.com>
2026-02-06 10:54:44 -0300

    README-fix

---
+++ lib/core/enums/game_state.dart
@@ -0,0 +1,22 @@
+/// Representar en qué estado se encuentra el juego en un momento dado.
+///
+/// Uso:
+/// El GameController (en la capa de presentación) usará este enum para saber qué pantalla mostrar.
+///
+/// Valores:
+/// - idle: El juego está en reposo (pantalla principal, esperando acción del usuario)
+/// - navigating: MapScreen (seleccionando nodo)
+/// - playing: El jugador está activamente respondiendo preguntas
+/// - loading: Cargando datos (Firestore, transiciones)
+/// - nodeCompleted: El jugador completó un nodo exitosamente
+/// - nodeFailed: El jugador falló un nodo (perdió una vida)
+/// - gameOver: El jugador perdió todas las vidas
+enum GameState {
+  idle,
+  navigating,
+  playing,
+  loading,
+  nodeCompleted,
+  nodeFailed,
+  gameOver
+}
